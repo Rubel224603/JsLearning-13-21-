b3dabfb06dd182ff75328ebdc02f8e7848b7f67d
Rubel Hosen <cserubelhosen@gmail.com>
2024-11-06 19:24:43 +0600

    Js learingin

---
+++ js/script.js
@@ -0,0 +1,82 @@
+//alert("hi");
+// var a=10; 
+// var  a=20;
+//let a = 10;
+//let a=30;
+//const ab=10;
+var ab=10;
+var ab='Var onek bar declare kora jai & value assing kora jai ';
+ab='10';
+document.write(ab);
+
+document.write("</br>")
+;
+let a = "rubel Hosen";
+ a= 10; //let  ek bar decalare kora jai ar value onek bar assign kora jai but onek bar decalre kora jabe na
+
+
+ a = "Atif Aslam";
+ a=200;
+ a='rohim';
+
+a=20;
+//document.write(a);
+//constant  :==only ekbar decalre kora jabe .ar ekbarei value assing kora jabe;
+
+ const b =  20;
+document.write("</br>"+b);
+console.log(b);
+console.log(typeof(a));
+
+var d = true;
+var e = ["Rubel",20,"jhenaidah",'Web Developer'];
+var f = {
+            name:"Rubel",
+            age:20,
+            address:'jhenaidah',
+            dream:"Web Developer"
+       };
+var g;
+var h=null;
+
+console.log(typeof(d));
+console.log(typeof(e));
+console.log(typeof(f));
+console.log(typeof(g));
+console.log(typeof(h));
+
+//operator;
+//arithmetical operator -,+,*,/,%, ++,--
+var ac=10; var bc= 20;
+console.log(ac + " "+ bc);
+ac--;// decriment operator
+
+console.log(ac--);
+console.log(ac);
+console.log(10%3); //modulas % operator
+
+//assignment operator; =,+=,-=,*=,/=,%=
+var sum = 10;
+var number = 10;
+ sum += number ;//sum = sum +number
+document.write("<br>"+sum);
+console.log(sum);
+
+sum %= number;
+console.log(sum);
+sum=20;
+sum/= number; //sum= sun/number
+console.log("division:= "+sum);
+var lNumber= 2;
+lNumber**=10;
+
+console.log(lNumber);
+var x=22;
+console.table(['rubel',25,'Computer','Web']);
+//console.error("soemthing went wrong");
+console.warn("here is warning");
+var y ='22';
+console.log(x===y);
+console.log(x===22);
+var n= 22;
+console.log(x===n);
